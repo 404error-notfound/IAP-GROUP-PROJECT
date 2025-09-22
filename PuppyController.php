940e53747644a4cf6f636ee54a7274ba425e401a
wilson mutua <mutuawilson85@gmail.com>
2025-09-22 13:46:22 +0300

    Create PuppyController.php

---
+++ PuppyController.php
@@ -0,0 +1,35 @@
+<?php
+// PuppyController.php
+class PuppyController {
+    private $puppyService;
+    private $userService;
+
+    public function __construct(PuppyService $puppyService, UserService $userService) {
+        $this->puppyService = $puppyService;
+        $this->userService = $userService;
+    }
+
+    public function browseAction($request) {
+        $filters = [
+            'breed' => $request['breed'] ?? null,
+            'minAge' => $request['min_age'] ?? null,
+            'maxAge' => $request['max_age'] ?? null
+        ];
+
+        $puppies = $this->puppyService->getAvailablePuppies($filters);
+        
+        // Render view with puppies data
+        include 'views/browse_puppies.php';
+    }
+
+    public function addPuppyAction($request) {
+        if (!$this->userService->isOwner()) {
+            throw new Exception("Only owners can add puppies");
+        }
+
+        $puppyData = $request['puppy'];
+        $puppyId = $this->puppyService->addNewPuppy($puppyData, $_SESSION['user_id']);
+        
+        header("Location: /puppy/details?id=" . $puppyId);
+    }
+}
