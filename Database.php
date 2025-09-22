f3ec845363d7bce5a119b61b28b8c23d68751bc8
wilson mutua <mutuawilson85@gmail.com>
2025-09-22 13:43:57 +0300

    Create Database.php

---
+++ Database.php
@@ -0,0 +1,22 @@
+<?php
+// Database.php (Singleton pattern)
+class Database {
+    private static $instance = null;
+    private $connection;
+
+    private function __construct() {
+        $this->connection = new PDO("mysql:host=localhost;dbname=go_puppy_go", "username", "password");
+        $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+
+    public static function getInstance() {
+        if (self::$instance == null) {
+            self::$instance = new Database();
+        }
+        return self::$instance;
+    }
+
+    public function getConnection() {
+        return $this->connection;
+    }
+}
